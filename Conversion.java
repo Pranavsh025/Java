c4a44bfd1f1cd41165c0fc6b463d291b9e8a0766
Pranavsh025 <pranavsharmapranav26@gmail.com>
2026-09-10 02:02:42 +0530

    Initial commit

---
+++ Conversion.java
@@ -0,0 +1,34 @@
+public class Conversion {
+    public static void main(String[] args) {
+        // // implicit conversion
+        // byte b = 24;
+        // int i;
+
+        // i = b; // implicit conversion from byte to int
+        // System.out.println(i); // 24
+
+        // // character to integer conversion
+        // char c = 'a';
+
+        // int a;
+
+        // a = c;
+        // System.out.println(a); // 97
+
+        // explicit conversion
+        // int i = 300;
+        // byte b; //-128 to +127
+
+        // b = (byte) i;
+        // System.out.println(b); // 300 % 256 = 44
+
+        // Truncating conversion
+        float f = 10.99f;
+
+        int i ;
+
+        i= (int)f;
+        System.out.println(i); // 10
+        
+        }
+}
---
+++ Variables.java
@@ -0,0 +1,28 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Integers --> byte , int , short , long 
+        //byte b = 0b101 ;
+        //byte b = 05;
+        byte b = 0xa;
+        short s = 10;
+        int i = 4000;
+        long l = 100000;
+
+        // Real numebers
+        float f = 10.54f ;
+        double d = 23.09876;
+
+        // characters 
+        char c = 'a';
+
+        // Boolean 
+        boolean bool = false;
+
+        System.out.println("integer values -->" + b +" , "+ s +" , "+ i +" , "+ l );
+        System.out.println("real number -->" + f +" , " + d);
+        System.out.println("Character-->" +c);
+        System.out.println("boolean values -->" +bool);
+
+}
+
+}
