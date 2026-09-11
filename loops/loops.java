713167355f275a249b8e93e55d5d3287276303ac
Pranavsh025 <pranavsharmapranav26@gmail.com>
2026-09-11 10:03:53 +0530

    added new file

---
+++ loops/loops.java
@@ -0,0 +1,27 @@
+package loops;
+public class loops {
+    public static void main(String[] args) {
+        //loops 
+
+        int i = 0;
+        //while loop
+        // while(i++ < 10){
+        //     System.out.println(i);
+        // }
+
+        // do while loop
+        // use of do while loop is when we want to execute the loop at least once even if the condition is false
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<=10);
+
+        // menu item selection using do while loop
+        /*
+        play game
+        return save game
+        exit
+         */
+
+    }
+}
---
+++ loops/loops2.java
@@ -0,0 +1,31 @@
+package loops;
+public class loops2 {
+    public static void main(String[] args) {
+        // for loop 
+        // for (int i = 1; i<=10; i++){
+        //     System.out.println(i);
+        // }
+
+        // for loop with a different increment
+        // for (int i = 10; i>=1; i--){
+        //     System.out.println(i);
+        // }
+
+        // nested for loop
+        // for (int i = 1; i<=5; i++){
+        //     for (int j = 1; j<=i; j++){
+        //         System.out.print("* ");
+        //     }
+        //    System.out.println();
+        // }
+
+        // jump statement in java
+        // break and continue
+        // for (int i = 1;i<=10;i++){
+        //     System.out.println(i);
+
+        //     if(i > 5){
+        //         break;
+        //     }
+        }
+    }
